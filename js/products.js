c5cb49ae11f869fd25adb010e73b809c0b516408
LOUNARA-TN <lounara990@gmail.com>
2026-03-04 19:35:45 +0100

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,8 +1,5 @@
 fetch('json/products.json')
-.then(res=>{
-  if(!res.ok) throw new Error('Impossible de charger le JSON'); 
-  return res.json();
-})
+.then(res=>res.json())
 .then(products=>{
   const container=document.querySelector('.grid');
   function displayProducts(filter='All'){
@@ -16,18 +13,11 @@ fetch('json/products.json')
       carousel.className='carousel';
       p.images.forEach((imgSrc,i)=>{
         const img=document.createElement('img'); 
-        img.dataset.src=imgSrc;
-        if(i===0) img.classList.add('active');
+        img.src=imgSrc;
         img.alt=p.name+' - vue '+(i+1);
+        if(i===0) img.classList.add('active');
         carousel.appendChild(img);
       });
-
-      const firstImg=new Image(); firstImg.src=p.images[0];
-      carousel.querySelectorAll('img').forEach(img=>img.src=img.dataset.src);
-
-      const imgs=carousel.querySelectorAll('img'); let current=0;
-      function showNext(){ imgs.forEach(img=>img.classList.remove('active')); imgs[current].classList.add('active'); current=(current+1)%imgs.length; }
-      setInterval(showNext,3000);
 
       const title=document.createElement('h3'); title.textContent=p.name;
       const desc=document.createElement('p'); desc.textContent=p.description;
@@ -47,8 +37,4 @@ fetch('json/products.json')
       displayProducts(link.dataset.category);
     });
   });
-})
-.catch(err=>{
-  document.querySelector('.grid').innerHTML=`<p style="text-align:center;color:red;">Erreur: ${err.message}</p>`;
-  console.error(err);
 });
